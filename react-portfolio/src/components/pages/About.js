dfd10cba17ed349d300ddd3892be1cfdd4979c50
brett hockridge <bretthockridge98@gmail.com>
2022-06-05 19:28:02 -0700

    small changes

--- react-portfolio/src/components/pages/About.js
+++ react-portfolio/src/components/pages/About.js
@@ -4,33 +4,16 @@ import profilePic from "../../assets/Bretts-selfie.jpg";
 export default function About() {
   return (
     <div id="about">
-      <img id="jason" alt="picture of Jason Yoo" src={profilePic} />
-      <h1><span id="nameSpan">Hi, I'm Jason!</span></h1>
+      <img id="Brett" alt="picture of Brett Hockridge" src={profilePic} />
+      <h1><span id="nameSpan">Hi, I'm Brett!</span></h1>
       <p>
-        My name is Jason Yoo and I am a Full-Stack Web Developer. I am well
-        versed in HTML5, CSS3, Multiple CSS-Frameworks, JavaScript, JQuery,
-        Node.js, Express.js, Handlebars.js, MySQL, Sequelize, MongoDB, Mongoose,
-        and React.js. As a web developer, I emphasize a mobile-first web
-        development perspective along with creating responsive web applications
-        which enhance the users experience.
+        {/* about me */}
       </p>
       <p>
-        I graduated from the California State University of Fullerton with a
-        B.A. in History with a minor in Asian American Studies. I also completed
-        the Full-Stack Web Development Program through the University of
-        California, Irvine. My career history includes experience as a Personal
-        Banker with Wells Fargo Bank N.A., I was also an accomplished English
-        teacher with the Seoul Metropolitan Office of Education. I am at present
-        a private consultant with a start up company located in Los Angeles,
-        Curien LLC. I graduated with top marks from the coding program at UC
-        Irvine.
+        {/* school */}
       </p>
       <p>
-        I am a motivated and dedicated software developer, with an eagerness to
-        continuing expanding my learning and growth as a web developer.
-        Leveraging my previous experiences in banking, teaching and consulting
-        with my now honed web development skills, I am excited to be a part of a
-        fast-paced, quality driven web development team.
+        {/* developer */}
       </p>
     </div>
   );
